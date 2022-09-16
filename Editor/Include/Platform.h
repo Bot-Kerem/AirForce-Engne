ecce92f3bbec831f906a451fde3696994c0de675
Bot-Kerem <akerem1434@gmail.com>
2022-09-16 19:24:08 +0300

    Added Platform Class for spesific platforms and added OpenFileDialog

---
+++ Editor/Include/Platform.h
@@ -0,0 +1,13 @@
+// project: AirForce
+// created by: Bot-Kerem
+// create date: 16.09.2022
+// updated: 16.09.2022
+
+#ifndef AIRFORCE_EDITOR_PLATFORM
+#define AIRFORCE_EDITOR_PLATFORM
+
+#include <string>
+
+void OpenFolderDialog(std::string& Path);
+
+#endif // AIRFORCE_EDITOR_PLATFORM
